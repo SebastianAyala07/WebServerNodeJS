dc884363d7493cf3c02b8dccfd26c917bc2d2a39
Sebastian Ayala <sebaxayala@gmail.com>
2021-07-23 21:51:56 -0500

    [ADD] Preparando WebServer para el despliegue

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const hbs = require('hbs');
+
+const app = express();
+const port = process.env.PORT;
 
 // Handlerbars
 app.set('view engine', 'hbs');
@@ -35,4 +38,4 @@ app.get('*', ( req, res ) => {
     res.sendFile(__dirname + '/public/404.html');
 });
 
-app.listen(8080);
+app.listen(port);
